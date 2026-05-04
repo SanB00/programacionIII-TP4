0a228da8c1a9e3401628eb82035cc504c2adcdd7
SantiB <151892199+SanB00@users.noreply.github.com>
2026-05-04 17:14:10 -0300

    de Derly Yulieth Valencia Fajardo - lunes, 4 de mayo de 2026, 16:51 hola, cargue los temas en el DropDownList.
     TP4Grupo18-v3.3.zip

--- TP4Grupo18/Ejercicio3.aspx.cs
+++ TP4Grupo18/Ejercicio3.aspx.cs
@@ -1,11 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace TP4Grupo18
 {
-    public partial class Ejercicio3 : System.Web.UI.Page
+    public partial class WebForm1 : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e) {
+        //string LibreriaSQL = "Data Source=localhost;Initial Catalog=Neptuno;Integrated Security=True";
+        string LibreriaSQL = "Data Source=DESKTOP-RFDMNU2\\SQLEXPRESS;Initial Catalog=Libreria;Integrated Security=True;Encrypt=False;TrustServerCertificate=True"; //yulieth
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+              
+                string consultaSQL = "SELECT * FROM Temas";
+                SqlConnection sqlConnection = new SqlConnection(LibreriaSQL);
+                sqlConnection.Open();
 
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(consultaSQL, sqlConnection);
+                DataSet dataSet = new DataSet();
+
+                sqlDataAdapter.Fill(dataSet, "TablaTemas");
+
+                ddlTemas.DataSource = dataSet.Tables["TablaTemas"];
+                ddlTemas.DataTextField = "Tema";
+                ddlTemas.DataValueField = "IdTema";
+                ddlTemas.DataBind();
+
+                sqlConnection.Close();
+
+                
+
+            
+            }
         }
     }
 }
