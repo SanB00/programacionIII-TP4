8441248103053ffbbe0e8fc055297a5ebc13b5f8
SantiB <151892199+SanB00@users.noreply.github.com>
2026-05-04 17:56:42 -0300

    ej3: Muestra resultados DB

--- TP4Grupo18/Ejercicio3B.aspx.cs
+++ TP4Grupo18/Ejercicio3B.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,8 +10,23 @@ namespace TP4Grupo18
 {
     public partial class Ejercicio3B : System.Web.UI.Page
     {
+#pragma warning disable IDE1006 // Naming Styles
         protected void Page_Load(object sender, EventArgs e) {
+#pragma warning disable IDE1006 // Naming Styles
+            if (!IsPostBack) {
+                //string idTemaSeleccionado =
+                cargarLibrosPorTema();
+            }
+        }
 
+        private void cargarLibrosPorTema(string idTema = "1") {
+            string consultaSQL = $"SELECT * FROM Libros WHERE idTema = {idTema}";
+            string cadenaConexion = new ConexionBBDD().obtenerCadenaDeConexion("Libreria");
+            DataTable tablaLibros = new ConexionBBDD().obtenerTablaDeLaBaseDeDatos(consultaSQL, cadenaConexion);
+            gvLibros.DataSource = tablaLibros;
+            gvLibros.DataBind();
+
+            lblCantResultados.Text = $"Hay {tablaLibros.Rows.Count} resultado/s";
         }
     }
 }
